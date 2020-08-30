7490f46fb2ff1bc4ae5386ffa069c4bf3f17de2a
Test <ihelpmotivate@gmail.com>
2020-08-30 01:52:45 -0400

    This is the first new file setup

---
+++ script.js
@@ -0,0 +1,46 @@
+
+/*
+Output
+	totalVolume
+Input
+	sets
+	reps
+	platesSide
+	barWeight
+	totalPlatesWeight
+	totalWeight
+	totalVolume
+Process
+	Read  sets user did
+	Read  reps the user did
+	Read  weight on one side of bar
+	Read weight of the bar
+	Get total weight of plates 	
+	Get total weight
+	Get total volume
+	Display total volume
+Algorithm
+	Read  sets user did
+	Read  reps the user did
+	Read  weight on one side of bar
+	Read weight of the bar
+	Get total weight of plates
+		platesSide * 2
+	Get total weight
+		totalPlatesWeight + barWeight
+	Get total volume
+		sets*reps*totalWeight
+	Display total volume
+
+	
+*/
+
+var sets;
+var reps;
+var platesSide;
+var barWeight;
+var totalPlatesWeight;
+var totalWeight;
+var totalVolume;
+
+document.write("hello world");
