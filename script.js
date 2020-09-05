c7f595f82c0e904f3a2449f5d3318fc7dbb8aab2
Test <ihelpmotivate@gmail.com>
2020-09-05 17:37:28 -0400

    Ver 1.6.1 Tried cleaning ver 1.6 need clear directions

--- script.js
+++ script.js
@@ -44,80 +44,83 @@ Object Oriented
 What I did 
 	Turned the methods back into regular functions
 	Created a constructer function called workout
-	Create an instance(example) of the workout called user
+	Create an instance(example) object of the workout called user
 	Passed the return value of each function
 	
 	Removed the steps where I previously assigned a method to a property name
 */
 
-
-
-
-
-	// functions for getting workout values from users
-	
-		
-		 function getSets (){ //added function as method 
-			var sets;
-			
-			sets = parseInt(prompt("How many sets did you do or are you doing?"));
-			return sets;
-			}
-		 
-		 function getReps (){ //added function as method 
-			var reps;
-			
-			reps = parseInt(prompt("How many reps did you do or are you doing?"));
-			return reps;
-			}
-		 
-		 function getPlatesSide (){
-			platesSide = parseInt(prompt("What is the weight of the plates on one side?"));
-			return platesSide;
-			}
-		 
-		 function getBarWeight () {
-			var barWeight;	
-			barWeight = parseInt(prompt("What is the bar weight?"));
-			return barWeight;
-			}
-		
-	
-	
 	// contructor function for workout
 	// input how many sets, reps, plates on one side, and the bar weight
 	// create attributes
+	
+
+
+	// constuctor function for getting workout values from users
+
 	function workout (getSets, getReps, getPlatesSide, getBarWeight){
 		this.Sets = getSets;
 		this.Reps = getReps;
 		this.PlatesSide = getPlatesSide;
 		this.BarWeight = getBarWeight;
-	}
+		}
 	
-	
+	//Create an object called user and created methods using functions as arguments
 	var user = new workout(getSets(), getReps(), getPlatesSide(), getBarWeight());
 	
+	
+	document.write("Your total volume is " + calculateVolume(user.PlatesSide, user.BarWeight,user.Sets, user.Reps) + " pounds.");
+	
+	
+	
+	
+	//Function for calculating volume
+	// Input take in plates, the weight of the bar, how many sets, reps
+	// output the total volume or work created
+	function calculateVolume (platesSide, barWeight,sets, reps){
 
-// Input take in plates, the weight of the bar, how many sets, reps
-// output the total volume or work created
-function calculateVolume (platesSide, barWeight,sets, reps){
 	var totalPlatesWeight;
 	var totalWeight;
 	var totalVolume;
-	
+
 	totalPlatesWeight = platesSide * 2;
 	totalWeight = totalPlatesWeight + barWeight;
 	totalVolume = sets * reps * platesSide;
 	return totalVolume;
-}
+	}
 
-/*
-user.Sets = user.getSets();
-user.Reps = user.getReps();
-user.PlatesSide = user.getPlatesSide();
-user.BarWeight = user.getBarWeight();
+	
+	
+	function getSets (){ 
+		var sets;
+		
+		sets = parseInt(prompt("How many sets did you do or are you doing?"));
+		return sets;
+		}
+	 
+	function getReps (){ 
+		var reps;
+		
+		reps = parseInt(prompt("How many reps did you do or are you doing?"));
+		return reps;
+		}
+	 
+	function getPlatesSide (){
+		platesSide = parseInt(prompt("What is the weight of the plates on one side?"));
+		return platesSide;
+		}
+	 
+	function getBarWeight () {
+		var barWeight;	
+		barWeight = parseInt(prompt("What is the bar weight?"));
+		return barWeight;
+		}
+		
 
-*/
+	
+	
 
-	document.write("Your total volume is " + calculateVolume(user.PlatesSide, user.BarWeight,user.Sets, user.Reps) + " pounds.");
+	
+
+	
 	
